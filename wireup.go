a476e14a756724a5fc1e539267f3978daabbe9c1
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2017-11-16 17:57:54 -0700

    Ability to receive notification of configuration changes.

--- wireup.go
+++ wireup.go
@@ -6,9 +6,10 @@ import (
 )
 
 type Wireup struct {
-	reader  Reader
-	storage Storage
-	signals []os.Signal
+	reader      Reader
+	storage     Storage
+	signals     []os.Signal
+	subscribers []chan interface{}
 }
 
 func New(reader Reader) *Wireup {
@@ -19,17 +20,25 @@ func New(reader Reader) *Wireup {
 }
 
 func (this *Wireup) WithSignal(signals ...os.Signal) *Wireup {
-	for _, item := range signals {
-		this.signals = append(this.signals, item)
-	}
+	this.signals = append(this.signals, signals...)
+	return this
+}
 
+func (this *Wireup) WithNotify(subscribers ...chan interface{}) *Wireup {
+	this.subscribers = append(this.subscribers, subscribers...)
 	return this
 }
 
 func (this *Wireup) Initialize() (Storage, error) {
-	reader := NewReader(this.reader, this.storage)
+	var reader Reader
+
+	reader = NewReader(this.reader, this.storage)
 	if _, err := reader.Read(); err != nil {
 		return nil, err
+	}
+
+	if len(this.subscribers) > 0 {
+		reader = NewNotifyReader(reader).Subscribe(this.subscribers...)
 	}
 
 	subscriber := NewSubscriber()
